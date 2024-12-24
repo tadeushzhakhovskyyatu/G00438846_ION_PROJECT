389d9975a713b98c2729dc98db5cf6275c6d1bc0
G00438846 <168219343+tadeushzhakhovskyyatu@users.noreply.github.com>
2024-12-24 20:21:47 +0000

    2

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '​G00438846',
+  webDir: 'www'
+};
+
+export default config;
